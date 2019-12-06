7e0c805ec37b3346989d2bb7c2165bdc03222c65
TheOnly8Z <13565172+TheOnly8Z@users.noreply.github.com>
2019-12-05 19:03:04 -0500

    Granny weapons

---
+++ lua/act3bm/merchant/sh_granny_weapons.lua
@@ -0,0 +1,130 @@
+-- Pistols
+ACT3_BlackMarket:LoadItemToMerchant({ -- C96
+   merchant = "Granny",
+   category = "Weapons",
+   class = "act3_c96",
+   quantity = 1,
+   saletype = "WPN",
+   cost = 75,
+})
+
+ACT3_BlackMarket:LoadItemToMerchant({ -- P1
+   merchant = "Granny",
+   category = "Weapons",
+   class = "act3_p1",
+   quantity = 1,
+   saletype = "WPN",
+   cost = 100,
+})
+
+ACT3_BlackMarket:LoadItemToMerchant({ -- K98 Obrez
+   merchant = "Granny",
+   category = "Weapons",
+   class = "act3_k98_obrez",
+   quantity = 1,
+   saletype = "WPN",
+   cost = 75,
+})
+
+-- Carbines/SMGs
+ACT3_BlackMarket:LoadItemToMerchant({ -- C96 Carbine
+   merchant = "Granny",
+   category = "Weapons",
+   class = "act3_c96_carbine",
+   quantity = 1,
+   saletype = "WPN",
+   cost = 125,
+})
+
+ACT3_BlackMarket:LoadItemToMerchant({ -- M712
+   merchant = "Granny",
+   category = "Weapons",
+   class = "act3_c96_m712",
+   quantity = 1,
+   saletype = "WPN",
+   cost = 175,
+})
+
+ACT3_BlackMarket:LoadItemToMerchant({ -- MP40
+   merchant = "Granny",
+   category = "Weapons",
+   class = "act3_mp40",
+   quantity = 1,
+   saletype = "WPN",
+   cost = 300,
+})
+
+
+-- Rifles
+ACT3_BlackMarket:LoadItemToMerchant({ -- K98
+   merchant = "Granny",
+   category = "Weapons",
+   class = "act3_k98",
+   quantity = 1,
+   saletype = "WPN",
+   cost = 400,
+})
+
+ACT3_BlackMarket:LoadItemToMerchant({ -- Musket
+   merchant = "Granny",
+   category = "Weapons",
+   class = "act3_musket",
+   quantity = 1,
+   saletype = "WPN",
+   cost = 200,
+})
+
+ACT3_BlackMarket:LoadItemToMerchant({ -- K98 Sniper
+   merchant = "Granny",
+   category = "Weapons",
+   class = "act3_k98_sniper",
+   quantity = 1,
+   saletype = "WPN",
+   cost = 600,
+})
+
+ACT3_BlackMarket:LoadItemToMerchant({ -- Tankgewehr
+   merchant = "Granny",
+   category = "Weapons",
+   class = "act3_tgewehr",
+   quantity = 1,
+   saletype = "WPN",
+   cost = 800,
+})
+
+-- Assault Rifles
+ACT3_BlackMarket:LoadItemToMerchant({ -- StG44
+   merchant = "Granny",
+   category = "Weapons",
+   class = "act3_stg44",
+   quantity = 1,
+   saletype = "WPN",
+   cost = 500,
+})
+
+ACT3_BlackMarket:LoadItemToMerchant({ -- StG44 PDW
+   merchant = "Granny",
+   category = "Weapons",
+   class = "act3_stg44_pdw",
+   quantity = 1,
+   saletype = "WPN",
+   cost = 450,
+})
+
+ACT3_BlackMarket:LoadItemToMerchant({ -- StG44 Carbine
+   merchant = "Granny",
+   category = "Weapons",
+   class = "act3_stg44_carbine",
+   quantity = 1,
+   saletype = "WPN",
+   cost = 475,
+})
+
+ACT3_BlackMarket:LoadItemToMerchant({ -- StG44 Sniper
+   merchant = "Granny",
+   category = "Weapons",
+   class = "act3_stg44_ssg44",
+   quantity = 1,
+   saletype = "WPN",
+   cost = 550,
+})
